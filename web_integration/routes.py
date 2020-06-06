6a30ed0aa3301caef8a0bf7c4d345b2dcd1e1605
neelu0 <neelu0@protonmail.com>
2020-06-06 22:03:48 +0500

    dummy patient profile added

--- web_integration/routes.py
+++ web_integration/routes.py
@@ -1,5 +1,5 @@
 from web_integration import app, rest_link
-from flask import render_template, abort
+from flask import render_template, abort, request
 import requests
 from web_integration.utilfuncs import time_con
 
@@ -18,3 +18,14 @@ def patients():
         abort(500)
     patient_list = r.json()
     return render_template('patients.html',title='Patients',page_title='Patients',breadcrumb_title='Patients',patient_list=patient_list, time_con=time_con,rest_link=rest_link)
+
+@app.route('/patients/<patient_id>', methods=['GET','POST'])
+def patient(patient_id):
+    if request.method == 'GET':
+        r = requests.get(f'{rest_link}/patients/{patient_id}')
+        if r.status_code == 404:
+            abort(404)
+        if r.status_code != 200:
+            abort(500)
+        patient_data = r.json()
+        return render_template('patient.html')
